44af2edd08c515b38cf37f8d485b123974812d19
Yash-Takhtani <iamyashtakhtani@gmail.com>
2026-09-04 22:05:50 +0530

    Fixed firmware

--- Firmware/4d_tracker/4d_tracker.ino
+++ Firmware/4d_tracker/4d_tracker.ino
@@ -15,20 +15,31 @@ const char* password = "YOUR_WIFI_PASSWORD";
 #define TFT_CS 6
 // #define TFT_BL -1
 
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+class MyST7789 : public Adafruit_ST7789 {
+public:
+  MyST7789(int8_t cs, int8_t dc, int8_t mosi, int8_t sclk, int8_t rst)
+    : Adafruit_ST7789(cs, dc, mosi, sclk, rst) {}
+
+  void setOffsets(uint8_t col, uint8_t row) {
+    _colstart = _colstart2 = col;
+    _rowstart = _rowstart2 = row;
+  }
+};
+
+MyST7789 tft = MyST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 3600); 
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 3600);
 
 void setup() {
   Serial.begin(115200);
   tft.init(76, 284);
-  // tft.setColRowStart(82, 18);
-  // tft.setOffsets(82, 18);
+  tft.setOffsets(82, 18);
   tft.invertDisplay(false);
   tft.setRotation(1);
   tft.fillScreen(ST77XX_BLACK);
   Serial.println("TFT Initialized!");
-  tft.setCursor(0,0);
+  tft.setCursor(0, 0);
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -41,7 +52,7 @@ void setup() {
 
 void loop() {
   timeClient.update();
-  tft.fillRect(0, 0, 200, 30, ST77XX_BLACK); 
+  tft.fillRect(0, 0, 200, 30, ST77XX_BLACK);
   tft.setCursor(0, 0);
   tft.print(timeClient.getFormattedTime());
   delay(500);
